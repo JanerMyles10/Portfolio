aebf84e2b3d70f7e32ee52990960f801438dc7cb
Janer Myle <janermyle11@gmail.com>
2025-08-04 13:00:22 +0800

    clickable-image

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header,RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
